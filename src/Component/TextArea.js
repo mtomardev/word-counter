92e0d89694caa1d2a112882f0bd056c7e6c5e2da
mtomardev <120013813+mtomardev@users.noreply.github.com>
2023-06-23 15:13:49 +0530

    second change

--- src/Component/TextArea.js
+++ src/Component/TextArea.js
@@ -14,6 +14,21 @@ const TextArea = (props) =>{
         setUpdatedtext(newtext)
     }
 
+    
+
+        let Fcapital = () =>{
+
+            const words = text.split(" ");
+
+            for (let i = 0; i < words.length; i++) {
+                words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+            }
+
+            let string = words.join(" ");
+        
+            setUpdatedtext(string)
+    }
+
     let ExtraSpace = () =>{
         let newtext = text.split(/[ ]+/).join(" ") ;
         setUpdatedtext(newtext)
@@ -38,10 +53,11 @@ let Copy = () =>{
 
 <button className="btn btn-primary " onClick={uppercase}>Convert To Upper Case</button>
 <button className="btn btn-primary mx-2 " onClick={LowerCase}>Convert To Lower Case</button>
+<button className="btn btn-primary" onClick={Fcapital}>First character Capital</button>
 <button className="btn btn-primary mx-2 " onClick={ExtraSpace}>Remove Extra Spaces</button>
-<button className="btn btn-primary mx-2 " onClick={Copy}>Copy Text</button>
-<button className="btn btn-primary " onClick={Reset}>Reset</button>
-<div className="output">
+<button className="btn btn-primary  " onClick={Copy}>Copy Text</button>
+<button className="btn btn-primary mx-2 my-3" onClick={Reset}>Reset</button>
+<div className="output ">
 <h6>Total: Words : {text.split(/\s+/).filter((element)=>{return element.length!== 0}).length} & Characters length with space :  {text.trim().length}</h6>        
 <textarea className="form-control" rows="6" value={updatedtext}></textarea>
 </div>
